f4e2cf30ed5464846397f17cfd1fbd2c08839d50
krolh <nguyenngochuyen10dgialoc@gmail.com>
2025-06-15 20:53:23 +0700

    loi

--- EcommerceBackend/Program.cs
+++ EcommerceBackend/Program.cs
@@ -66,6 +66,8 @@ builder.Services.AddCors(options =>
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddAuthorization();
+//builder.Services.AddControllers();
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
@@ -100,7 +102,10 @@ builder.Services.AddSwaggerGen(c =>
         }
     });
 });
-builder.WebHost.UseWebRoot("wwwroot");
+//builder.WebHost.UseWebRoot("wwwroot");
+builder = WebApplication.CreateBuilder(new WebApplicationOptions {
+    WebRootPath = "wwwroot"
+});
 
 var app = builder.Build();
 
